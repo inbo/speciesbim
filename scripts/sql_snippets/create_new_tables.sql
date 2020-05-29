9d7373f02fb43595f2e57d90fb2e2fd063e40272
Nicolas Noé <nicolas.noe@inbo.be>
2020-05-29 10:10:00 +0200

    Rename table gbiftaxonomy -> taxonomy (see #2)

--- scripts/sql_snippets/create_new_tables.sql
+++ scripts/sql_snippets/create_new_tables.sql
@@ -1,5 +1,5 @@
 -- That table contains taxonomic data from GBIF (obtained after matches on the content of scientificname table
-CREATE TABLE gbiftaxonomy (
+CREATE TABLE taxonomy (
     id serial PRIMARY KEY,  -- internal (to the DB) ID
     gbifId integer NOT NULL, -- ID at GBIF
     scientificName character varying(255), -- as returned by GBIF
@@ -10,7 +10,7 @@ CREATE TYPE gbifmatchtype AS ENUM ('EXACT', 'FUZZY', 'HIGHERRANK');
 -- table contains scientificnames in use in the database, a link to "taxonomy" and metadata about the taxonomic match at GBIF
 CREATE TABLE scientificname (
     id serial PRIMARY KEY,
-    taxonomyId integer REFERENCES gbiftaxonomy(id), -- Can be null if no match
+    taxonomyId integer REFERENCES taxonomy(id), -- Can be null if no match
     scientificName character varying(255) NOT NULL, -- as appear in the old "taxon" table
     authorship character varying(255), -- as appear in the old "taxon" table,
     -- !! the following field are attributes of the match process !!
--- scripts/sql_snippets/drop_new_tables_if_exists.sql
+++ scripts/sql_snippets/drop_new_tables_if_exists.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS scientificname;
 DROP TYPE IF EXISTS gbifmatchtype;
-DROP TABLE IF EXISTS gbiftaxonomy;
+DROP TABLE IF EXISTS gbiftaxonomy; -- Previous name of the taxonomy table, in case the DB is outdated
+DROP TABLE IF EXISTS taxonomy;
