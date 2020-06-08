c3bea23d4867ab8e833ace6420590a9e11c46211
damianooldoni <damiano.oldoni@inbo.be>
2020-06-08 22:34:12 +0200

    Add deprecated TaxonId to scientificname table

--- scripts/sql_snippets/populate_scientificname.sql
+++ scripts/sql_snippets/populate_scientificname.sql
@@ -1,5 +1,5 @@
-INSERT INTO scientificname ("scientificName", "authorship")
-SELECT acceptedname, scientificnameauthorship FROM biodiv.taxon WHERE
+INSERT INTO scientificname ("deprecatedTaxonId", "scientificName", "authorship")
+SELECT id, acceptedname, scientificnameauthorship FROM biodiv.taxon WHERE
 id IN (SELECT nptaxonid FROM biodiv.commontaxa) OR
 id IN (SELECT taxonid FROM biodiv.speciesannex) OR
 id IN (SELECT taxonid FROM biodiv.media) OR
