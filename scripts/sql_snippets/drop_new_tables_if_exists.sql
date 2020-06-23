dccbc50546799870fb06ef07f497dab1b2de2731
Nicolas Noé <nicolas.noe@inbo.be>
2020-06-23 14:15:52 +0200

    Create new table for vernacular names

--- scripts/sql_snippets/drop_new_tables_if_exists.sql
+++ scripts/sql_snippets/drop_new_tables_if_exists.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS vernacularname;
 DROP TABLE IF EXISTS scientificname;
 DROP TYPE IF EXISTS gbifmatchtype;
 DROP TABLE IF EXISTS gbiftaxonomy; -- Previous name of the taxonomy table, in case the DB is outdated
