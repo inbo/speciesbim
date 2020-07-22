9a183ae14700b8d822aa5587f6be032a6c62092c
damianooldoni <damiano.oldoni@inbo.be>
2020-07-22 15:11:49 +0200

    Merge branch 'master' into load_vernacularnames

--- scripts/sql_snippets/drop_new_tables_if_exists.sql
+++ scripts/sql_snippets/drop_new_tables_if_exists.sql
@@ -3,3 +3,4 @@ DROP TABLE IF EXISTS scientificname;
 DROP TYPE IF EXISTS gbifmatchtype;
 DROP TABLE IF EXISTS gbiftaxonomy; -- Previous name of the taxonomy table, in case the DB is outdated
 DROP TABLE IF EXISTS taxonomy;
+DROP TABLE IF EXISTS rank;
