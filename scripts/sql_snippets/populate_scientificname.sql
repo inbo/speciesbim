4a7291c58bf87497747bde81202c6c7c3fb239ad
damianooldoni <damiano.oldoni@inbo.be>
2020-10-02 11:34:03 +0200

    Do not insert speciesannex names in scientificname table
    As we use names in csv file https://github.com/inbo/speciesbim/blob/annexscientificname/data/raw/official_annexes.csv

--- scripts/sql_snippets/populate_scientificname.sql
+++ scripts/sql_snippets/populate_scientificname.sql
@@ -1,7 +1,6 @@
 INSERT INTO scientificname ("deprecatedTaxonId", "scientificName", "authorship")
 SELECT id, acceptedname, scientificnameauthorship FROM biodiv.taxon WHERE
 id IN (SELECT nptaxonid FROM biodiv.commontaxa) OR
-id IN (SELECT taxonid FROM biodiv.speciesannex) OR
 id IN (SELECT taxonid FROM biodiv.media) OR
 id IN (SELECT taxonid FROM biodiv.identifiablespecies WHERE
 id IN (SELECT identifiablespeciesid FROM biodiv.occurence))
